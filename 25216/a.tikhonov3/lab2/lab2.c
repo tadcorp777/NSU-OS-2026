dc66661ef0190fbaaa8b982051fe6d170126fe4e
Artem Tikhonov <a.tikhonov3.g.nsu.ru>
2026-09-17 11:06:20 +0700

    lab2 fix 2

--- 25216/a.tikhonov3/lab2/lab2.c
+++ 25216/a.tikhonov3/lab2/lab2.c
@@ -10,7 +10,7 @@ int main()
     time_t now;
     struct tm *sp;
 
-    (void)time(&now);
+    time(&now);
     printf("%s", ctime(&now));
 
     setenv("TZ", "PST8", 1);
@@ -18,7 +18,7 @@ int main()
 
     sp = localtime(&now);
     printf("%d/%d/%02d %d:%02d %s\n",
-           sp->tm_mon + 1, sp->tm_mday, sp->tm_year,
+           sp->tm_mon + 1, sp->tm_mday, sp->tm_year + 1900,
            sp->tm_hour, sp->tm_min, tzname[sp->tm_isdst]);
     exit(0);
 }
